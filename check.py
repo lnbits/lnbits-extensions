5ec88f5a32f8ca915d9f6d2be2097d50742476d1
Pavol Rusnak <pavol@rusnak.io>
2023-02-22 18:18:07 +0100

    improve check script
    - try to unpack zips, check file list for expected files
    - add optional caching via requests_cache
    - add exceptions for extensions that don't ship license/manifest yet

--- check.py
+++ check.py
@@ -1,13 +1,31 @@
-import io
 import json
-from hashlib import sha256
-from os.path import basename
 import sys
+from hashlib import sha256
+from io import BytesIO
+from os.path import basename
+from zipfile import ZipFile
 
-from PIL import Image
 import requests
+from PIL import Image
 
 EXTENSIONS_FILE = "extensions.json"
+UNPACK_ZIP_CONTENTS = False
+CHECK_ZIP_CONTENTS = True
+
+MANDATORY_FILES = [
+    "LICENSE",
+    "README.md",
+    "__init__.py",
+    "config.json",
+    "manifest.json",
+]
+
+# set to True to enable caching of downloaded files, useful for debug
+CACHE_DOWNLOADS = False
+if CACHE_DOWNLOADS:
+    import requests_cache
+
+    requests_cache.install_cache("lnbits-extensions-cache")
 
 
 def get_remote_file(url):
@@ -15,9 +33,10 @@ def get_remote_file(url):
     return r.content
 
 
-def get_remote_hash(url):
+def get_remote_zip(url):
     d = get_remote_file(url)
-    return sha256(d).hexdigest()
+    z = ZipFile(BytesIO(d))
+    return sha256(d).hexdigest(), z
 
 
 class Extension:
@@ -31,48 +50,89 @@ class Extension:
         self.archive = ext["archive"]
         self.hash = ext["hash"]
 
-    def validate(self):
+    def validate(self) -> (bool, str):
         print(f"Checking '{self.name}' extension ({self.id} {self.version})")
 
         # sanity checks
         if not self.name[0].isupper():
-            print("- ERROR: name does not start with uppercase letter")
-            assert False
+            return False, "name does not start with uppercase letter"
         if not self.short_description[0].isupper():
-            print("- ERROR: short_description does not start with uppercase letter")
-            assert False
+            return False, "short_description does not start with uppercase letter"
         if self.short_description.endswith("."):
-            print(f"- ERROR: short_description does end with '.'")
-            assert False
+            return False, "short_description should not end with '.'"
         if not self.id == self.id.lower():
-            print("- ERROR: id has mixed casing")
-            assert False
+            return False, "id has mixed casing"
         if not self.archive.startswith(self.repo):
-            print("- ERROR: archive URL does not start with repo URL")
-            assert False
+            return False, "archive URL does not start with repo URL"
         if not self.archive.endswith(f"{self.version}.zip"):
             bn = basename(self.archive)
-            print(f"- ERROR: archive '{bn}' does not end with version (expected: {self.version}.zip)")
-            assert False
+            return False, "archive name '{bn}' doesn't end with {self.version}.zip"
 
         # print archive info from json
-        hash_archive = get_remote_hash(self.archive)
+        archive_hash, archive_zip = get_remote_zip(self.archive)
         print(f"- url  : {self.archive}")
         print(f"- hash : {self.hash} (expected)")
-        print(f"- hash : {hash_archive} (real)")
+        print(f"- hash : {archive_hash} (real)")
+
+        # check downloaded zip
+        bad_file = archive_zip.testzip()
+        if bad_file is not None:
+            return False, f"archive check for file {bad_file} failed"
+
+        if UNPACK_ZIP_CONTENTS:
+            archive_zip.extractall("extensions")
+
+        if CHECK_ZIP_CONTENTS:
+            filelist = archive_zip.namelist()
+            prefix = f"{basename(self.repo)}-{self.version}"
+
+            mandatory_files = MANDATORY_FILES.copy()
+            # these are known to not include the LICENSE file in their latest release
+            # this is fixed in master, but we are waiting for the upcoming release
+            if self.id in [
+                "bleskomat",
+                "boltcards",
+                "deezy",
+                "discordbot",
+                "events",
+                "hivemind",
+                "livestream",
+                "market",
+                "nostrnip5",
+                "offlineshop",
+                "paywall",
+                "satsdice",
+                "smtp",
+                "streamalerts",
+                "subdomains",
+                "withdraw",
+            ]:
+                mandatory_files.remove("LICENSE")
+
+            # these are known to not include the manifest.json file in their latest release
+            # this is fixed in master, but we are waiting for the upcoming release
+            if self.id in [
+                "bleskomat",
+                "smtp",
+                "subdomains",
+            ]:
+                mandatory_files.remove("manifest.json")
+
+            for f in mandatory_files:
+                fn = f"{prefix}/{f}"
+                if fn not in filelist:
+                    return False, f"file {fn} not contained in the archive"
 
         # check icon
         try:
             icon = get_remote_file(self.icon)
-            img = Image.open(io.BytesIO(icon))
-            status = f"OK {img.size[0]}x{img.size[1]} @ {img.mode}"
-            icon_ok = True
-        except:
-            status = "BROKEN"
-            icon_ok = False
-        print(f"- icon : {status} ({self.icon})")
+            img = Image.open(BytesIO(icon))
+            print(f"- icon : OK {img.size[0]}x{img.size[1]} @ {img.mode} ({self.icon})")
+        except Exception as ex:
+            print(f"- icon : broken ({self.icon})")
+            return False, f"broken icon ({self.icon})"
 
-        return self.hash == hash_archive and icon_ok
+        return True, ""
 
 
 def main(args):
@@ -83,8 +143,8 @@ def main(args):
         extensions = j["extensions"]
         featured = j["featured"]
         print("OK")
-    except:
-        print("FAILED")
+    except Exception as ex:
+        print(f"FAILED: {ex}")
         return False
     print()
 
@@ -95,30 +155,31 @@ def main(args):
         for f in featured:
             assert f in extensions_ids
         print("OK")
-    except:
-        print("FAILED")
+    except Exception as ex:
+        print(f"FAILED: {ex}")
         return False
     print()
 
-    failed = []
+    failed = {}
     for ext in extensions:
         # skip extension if args are provided and extension mentioned
         if args and ext["id"] not in args:
             continue
         e = Extension(ext)
-        try:
-            assert e.validate()
+        ok, err = e.validate()
+        if ok:
             print("OK")
-        except:
-            print("FAILED")
-            failed.append(ext["id"])
+        else:
+            print(f"FAILED: {err}")
+            failed[ext["id"]] = err
         print()
 
-    print("Failed extensions: ", end="")
+    print("Failed extensions:")
     if not failed:
         print("NONE, ALL GOOD!")
     else:
-        print(" ".join(failed))
+        for k, v in failed.items():
+            print(f"- {k}: {v}")
 
     return not failed
 
