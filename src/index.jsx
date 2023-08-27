4d27ca633c4ae15c551312a411e15115898b005d
dni ⚡ <office@dnilabs.com>
2023-08-27 13:21:45 +0200

    [FEAT] add github pages website for extensions (#138)
    * [FEAT] add github pages website for extensions
    small solidjs app that parsees extensions.json and displays is nicely.
    autodeployed to github pages.
    added prettier

---
+++ src/index.jsx
@@ -0,0 +1,30 @@
+import { render } from "solid-js/web";
+import "./index.scss";
+import data from "../extensions.json";
+
+render(
+    () => (
+        <div id="extension-list">
+            <For each={data.extensions}>
+                {(ext) => (
+                    <div class="extension">
+                        <a target="_blank" href={ext.repo}>
+                            <img src={ext.icon} alt={ext.id} />
+                        </a>
+                        <h2>{ext.id}</h2>
+                        <span class="version">{ext.version}</span>
+                        <p>{ext.short_description}</p>
+                        <a target="_blank" href={ext.repo}>
+                            github
+                        </a>
+                        <br />
+                        <a target="_blank" href={ext.archine}>
+                            download
+                        </a>
+                    </div>
+                )}
+            </For>
+        </div>
+    ),
+    document.getElementById("root")
+);
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+
+export default defineConfig({
+    plugins: [solidPlugin()],
+    server: {
+        cors: { origin: "*" },
+    },
+    build: {
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+    },
+});
