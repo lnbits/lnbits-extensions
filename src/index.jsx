7d6ca4f44eb221ef4c69c81d5a09877cb079c8c3
dni ⚡ <office@dnilabs.com>
2023-11-24 22:08:35 +0100

    feat: add min version to html page

--- src/index.jsx
+++ src/index.jsx
@@ -12,6 +12,7 @@ render(
                             <img src={ext.icon} alt={ext.id} />
                         </a>
                         <h2>{ext.id}</h2>
+                        <span class="min-version">{ext.min_lnbits_version || "0.0.0"}</span>
                         <span class="version">{ext.version}</span>
                         <p>{ext.short_description}</p>
                         <a class="btn" target="_blank" href={ext.repo}>
