47cbf037b90bff4e4cac1ea23bc2e3fb2dd1a451
dni ⚡ <office@dnilabs.com>
2024-03-28 11:54:26 +0100

    feat: add extension integration tests
    this is the new home for integration tests for extension.
    
    the main repo `lnbits/lnbits` will also use the main branch of the repo to run the same extension tests.

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+for file in $( ls ./integration/*.jmx); do
+    echo "Cleaning logs"
+    rm -r reports logs
+    echo "Running test with $file"
+    filename=$(basename "$file" ".jmx")
+    ./apache-jmeter-5.6.2/bin/jmeter -n -t $file -l logs/$filename.log -e -o reports ;
+    error_count=$(cat jmeter.log | grep -m 1 "summary =" | awk '{print $19}')
+    echo "Error count: '$error_count'"
+    echo "##########"
+    echo "$error_count" == "0"
+    echo "###########$error_count ###########"
+    if [ "$error_count" = "0" ]; then
+        echo "Test $filename OK."
+        rm -r reports/*
+    else
+        echo "Test $filename failed. Error count: '$error_count'."
+        exit 1
+    fi
+done
