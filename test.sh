e48d4df123c2e29d6cbf629629384456fcf4d79e
Vlad Stan <stan.v.vlad@gmail.com>
2024-05-28 17:34:43 +0300

    chore: add timestamp logs

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ for file in $files; do
     echo "Cleaning logs"
     rm -r reports logs
     echo "Running test with $file"
+    date
     filename=$(basename "$file" ".jmx")
     echo "Using Extension Manifest: $EXTENSIONS_MANIFEST_PATH"
     ./apache-jmeter-5.6.3/bin/jmeter -DextensionsManifestPath="$EXTENSIONS_MANIFEST_PATH" -n -t $file -l logs/$filename.log -e -o reports ;
@@ -20,4 +21,5 @@ for file in $files; do
         cat logs/$filename.log
         exit 1
     fi
+    date
 done
