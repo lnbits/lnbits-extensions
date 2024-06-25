fb6b6aedf6b62c252f9c27f3d0362110ed82d277
dni ⚡ <office@dnilabs.com>
2024-06-25 07:58:46 +0200

    feat: update ext page
    wip

--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,95 @@
 import { render } from "solid-js/web";
 import "./index.scss";
 import data from "../extensions.json";
+import metadata from "../metadata.json";
+import { Router, Route, useParams, A } from "@solidjs/router";
+import { FaSolidUsers, FaBrandsGithub, FaSolidDownload } from 'solid-icons/fa'
 
-render(
-    () => (
+const extensions = [
+    ...new Map(data.extensions.map((ext) => [ext.id, ext])).values(),
+];
+extensions.forEach((ext) => {
+    const meta = metadata.find((m) => m.id === ext.id);
+    if (meta) {
+        Object.assign(ext, meta);
+    }
+});
+
+const Details = () => {
+    const params = useParams();
+    const extensions = [
+        ...new Map(data.extensions.map((ext) => [ext.id, ext])).values(),
+    ];
+    const ext = extensions.find((ext) => ext.id === params.name);
+    return (
+        <div>
+            <h1>{ext.id}</h1>
+            <p>{ext.short_description}</p>
+        </div>
+    );
+};
+
+const Home = () => {
+    return (
         <div id="extension-list">
-            <For each={data.extensions}>
+            <For each={extensions}>
                 {(ext) => (
                     <div class="extension">
-                        <a target="_blank" href={ext.repo}>
-                            <img src={ext.icon} alt={ext.id} />
-                        </a>
-                        <h2>{ext.id}</h2>
-                        <span class="min-version">{ext.min_lnbits_version || "0.0.0"}</span>
-                        <span class="version">{ext.version}</span>
-                        <p>{ext.short_description}</p>
-                        <a class="btn" target="_blank" href={ext.repo}>
-                            github
-                        </a>
-                        <a class="btn" target="_blank" href={ext.archive}>
-                            download
-                        </a>
+                        <div class="ext-image">
+                            <A href={ext.id}>
+                                <img src={ext.icon} alt={ext.id} />
+                            </A>
+                        </div>
+                        <div class="ext-desc">
+                            <h2>
+                                <A href={ext.id}>{ext.name}</A>
+                                <span class="min-version">
+                                    {ext.min_lnbits_version || "0.0.0"}
+                                </span>
+                                <span class="version">{ext.version}</span>
+                            </h2>
+                            <p>{ext.short_description}</p>
+                                <span title="contributors" class="contributors"><FaSolidUsers /> {ext.contributors.length}</span>
+                            <a class="btn" title="github" target="_blank" href={ext.repo}>
+                                <FaBrandsGithub />
+                            </a>
+                            <a class="btn" title="download" target="_blank" href={ext.archive}>
+                                <FaSolidDownload />
+                            </a>
+                        </div>
                     </div>
                 )}
             </For>
         </div>
+    );
+};
+
+const App = (props) => (
+    <>
+        <header>
+            <A href="/">
+                <img class="logo" src="./logo.svg" alt="LNbits Logo" />
+            </A>
+            <h2>Extension Repository</h2>
+        </header>
+        {props.children}
+        <footer>
+            LNbits, free and open-source Lightning wallet and accounts system.
+            <a
+                target="_blank"
+                href="https://github.com/lnbits/lnbits-extensions">
+                github
+            </a>
+        </footer>
+    </>
+);
+
+render(
+    () => (
+        <Router root={App}>
+            <Route path="/:name" component={Details} />
+            <Route path="/" component={Home} />
+        </Router>
     ),
-    document.getElementById("root")
+    document.getElementById("root"),
 );
