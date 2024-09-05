1e6a0f4d15613f4f13e84f0825b8698aec9e801d
dni ⚡ <office@dnilabs.com>
2024-09-05 15:42:58 +0200

    feat: max_lnbits_version for older releases (#397)

--- util.sh
+++ util.sh
@@ -50,6 +50,8 @@ update_extension() {
     archive="https://github.com/lnbits/$1/archive/refs/tags/$2.zip"
     wget -q --spider $archive || { echo "ERROR: release does not exist" ; exit 1 ; }
     update_extension_attribute $1 version $(echo $2 | sed -r "s/v//")
+    # set max_version to 0, which means there is no max version for latest release
+    update_extension_attribute $1 max_lnbits_version 0
     update_extension_attribute $1 archive $archive
     update_extension_attribute $1 hash $(wget -O - $archive 2> /dev/null | sha256sum | cut -d" " -f 1)
 }
